34efc28a297a04e6bbc6da2aea46cfc3718bbd31
varen1994 <varendersingh130@gmail.com>
2025-11-07 21:08:27 +0530

    - home page base project added

---
+++ AtlysHomePageDesign/AtlysHomePageDesign/AtlysHomePageDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlysHomePageDesignApp.swift
+//  AtlysHomePageDesign
+//
+//  Created by varender singh on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AtlysHomePageDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
